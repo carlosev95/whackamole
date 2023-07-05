fa1ab4cbcb9ccacff1ff7f7a425de2bbaa4a75eb
Carlos E <cromo_lp@hotmail.com>
2023-07-04 23:07:24 -0600

    Añadimos el evento para que el puntero cambie de forma al darle click a un mole

--- js/game.js
+++ js/game.js
@@ -1,72 +1,81 @@
 // Variables del juego
-let timer = 10;
-let score = 0;
-let intervalId;
+let timer = 10
+let score = 0
+let intervalId
 let finalMessage = document.getElementById("final-message")
 
 // Obtener elementos del DOM
-const startButton = document.getElementById("startButton");
-const holes = document.getElementsByClassName("hole");
-const moles = document.getElementsByClassName("mole");
-const timerDisplay = document.getElementById("timer");
-const scoreDisplay = document.getElementById("score");
+const startButton = document.getElementById("startButton")
+const moles = document.getElementsByClassName("mole")
+const timerDisplay = document.getElementById("timer")
+const scoreDisplay = document.getElementById("score")
+const elemento = document.getElementById("game-container")
+
+//Evento click para cambiar imagen del puntero
+elemento.addEventListener('mousedown', function() {
+    elemento.style.cursor = 'url(../img/mazo_click.png), auto';
+})
+
+elemento.addEventListener('mouseup', function() {
+    elemento.style.cursor = 'url(../img/mazo.png), auto';
+})
 
 // Función para iniciar el juego
 function startGame() {
-    startButton.disabled = true;
-    score = 0;
-    timer = 10;
-    timerDisplay.textContent = `Tiempo restante: ${timer}`;
-    scoreDisplay.textContent = `Topos golpeados: ${score}`;
+    startButton.disabled = true
+    score = 0
+    timer = 10
+    timerDisplay.textContent = `Tiempo restante: ${timer}`
+    scoreDisplay.textContent = `Topos golpeados: ${score}`
     
-    showRandomMole();
+    showRandomMole()
     
     intervalId = setInterval(() => {
-        timer--;
-        timerDisplay.textContent = `Tiempo restante: ${timer}`;
+        timer--
+        timerDisplay.textContent = `Tiempo restante: ${timer}`
         
         if (timer === 0) {
-            endGame();
+            endGame()
         } else {
-            showRandomMole();
+            showRandomMole()
         }
-    }, 1000);
+    }, 1000)
 }
 
 // Función para mostrar un topo aleatorio
 function showRandomMole() {
-    hideAllMoles();
+    hideAllMoles()
     
-    const randomIndex = Math.floor(Math.random() * holes.length);
-    const randomMole = moles[randomIndex];
-    randomMole.style.display = "block";
+    const randomIndex = Math.floor(Math.random() * moles.length)
+    const randomMole = moles[randomIndex]
+    randomMole.style.display = "block"
     
     // Evento de clic para golpear al topo
-    randomMole.addEventListener("click", hitMole);
+    randomMole.addEventListener('mousedown', hitMole)
 }
 
 // Función para ocultar todos los topos
 function hideAllMoles() {
     for (let mole of moles) {
-        mole.style.display = "none";
-        mole.removeEventListener("click", hitMole);
+        mole.style.display = "none"
+        mole.removeEventListener('mousedown', hitMole)
     }
 }
 
 // Función para golpear al topo
 function hitMole() {
-    score++;
-    scoreDisplay.textContent = `Topos golpeados: ${score}`;
-    this.style.display = "none";
+    score++
+    scoreDisplay.textContent = `Topos golpeados: ${score}`
+    this.style.display = "none"
 }
 
 // Función para finalizar el juego
 function endGame() {
-    clearInterval(intervalId);
-    hideAllMoles();
-    startButton.disabled = false;
+    clearInterval(intervalId)
+    hideAllMoles()
+    startButton.disabled = false
     finalMessage.innerText = "¡Fin del juego! Tu puntuación final es: "+ score
 }
 
 // Evento de clic para iniciar el juego
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", startGame)
